34a8d92efce93025e5b75bb10528787a477bb700
Sergio Acosta <sergio.acosta@gmail.com>
2020-05-28 02:09:25 -0500

    Concurrent directory walk, from 25 to 5 seconds

--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
 
 	"github.com/beevik/etree"
 	"github.com/scylladb/go-set/strset"
 )
+
+var wg sync.WaitGroup
+var xmlnsDeclarations = make(map[string]*strset.Set)
 
 func walkXML(e *etree.Element, af func(*etree.Element, etree.Attr)) {
 	for _, a := range e.Attr {
@@ -19,57 +23,67 @@ func walkXML(e *etree.Element, af func(*etree.Element, etree.Attr)) {
 	}
 }
 
-func main() {
+var lock = sync.Mutex{}
 
-	wd, err := os.Getwd()
+func saveDeclaration(prefix string, namespaceURI string) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	if xmlnsDeclarations[namespaceURI] == nil {
+		xmlnsDeclarations[namespaceURI] = strset.New(prefix)
+	} else {
+		xmlnsDeclarations[namespaceURI].Add(prefix)
+	}
+}
+
+func extractXmlnsDeclarations(path string) {
+	doc := etree.NewDocument()
+	err := doc.ReadFromFile(path)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Analyzing: %s\n", wd)
 
-	xmlnsDeclarations := make(map[string]*strset.Set)
-
-	saveDeclaration := func(prefix string, namespaceURI string) {
-		if xmlnsDeclarations[namespaceURI] == nil {
-			xmlnsDeclarations[namespaceURI] = strset.New(prefix)
-		} else {
-			xmlnsDeclarations[namespaceURI].Add(prefix)
+	root := doc.Root()
+	walkXML(root, func(e *etree.Element, a etree.Attr) {
+		if a.Space == "xmlns" {
+			saveDeclaration(a.Key, a.Value)
 		}
-	}
+	})
+}
 
-	extractXmlnsDeclarations := func(path string, err error) error {
-		doc := etree.NewDocument()
-		err = doc.ReadFromFile(path)
-		if err != nil {
-			return err
+func walkDir(dir string) {
+	defer wg.Done()
+
+	visit := func(path string, f os.FileInfo, err error) error {
+		if f.IsDir() && path != dir {
+			wg.Add(1)
+			go walkDir(path)
+			return filepath.SkipDir
 		}
-
-		root := doc.Root()
-		walkXML(root, func(e *etree.Element, a etree.Attr) {
-			if a.Space == "xmlns" {
-				saveDeclaration(a.Key, a.Value)
-			}
-		})
+		if f.Mode().IsRegular() && filepath.Ext(f.Name()) == ".xml" {
+			extractXmlnsDeclarations(path)
+		}
 		return nil
 	}
 
-	err = filepath.Walk(wd,
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			if filepath.Ext(path) == ".xml" {
-				return extractXmlnsDeclarations(path, err)
-			}
-			return nil
-		})
+	filepath.Walk(dir, visit)
+}
+
+func main() {
+
+	workingDir, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("Analyzing: %s\n", workingDir)
+
+	wg.Add(1)
+	walkDir(workingDir)
+	wg.Wait()
 
 	for namespace, prefixes := range xmlnsDeclarations {
 		log.Printf("[%v] \"%s\" declared as %s\n",
-			map[bool]string{true: "-OK!-", false: "-ERR-"}[prefixes.Size() == 1],
+			map[bool]string{true: "-OK!-", false: "-ERR-"}[(prefixes).Size() == 1],
 			namespace,
 			prefixes)
 	}
